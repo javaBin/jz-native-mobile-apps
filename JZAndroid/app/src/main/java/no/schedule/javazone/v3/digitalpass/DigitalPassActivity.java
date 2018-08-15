77d518158d7a871dba6b2f6e56784df20b708ab8
1npu7 <herman.netskar@gmail.com>
2018-08-15 11:28:51 +0200

    added classes and libraries for qr scanning

--- JZAndroid/app/src/main/java/no/schedule/javazone/v3/digitalpass/DigitalPassActivity.java
+++ JZAndroid/app/src/main/java/no/schedule/javazone/v3/digitalpass/DigitalPassActivity.java
@@ -1,39 +1,24 @@
 package no.schedule.javazone.v3.digitalpass;
 
 
-import android.app.AlertDialog;
-import android.content.DialogInterface;
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.text.SpannableString;
-import android.text.TextUtils;
-import android.text.method.LinkMovementMethod;
-import android.text.util.Linkify;
-import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.TextView;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.ValueEventListener;
 
 import no.schedule.javazone.v3.R;
-import no.schedule.javazone.v3.myschedule.MyScheduleDialogFragment;
 import no.schedule.javazone.v3.navigation.NavigationModel;
 import no.schedule.javazone.v3.schedule.ScheduleView;
 import no.schedule.javazone.v3.ui.BaseActivity;
 
-import static no.schedule.javazone.v3.util.LogUtils.LOGD;
 import static no.schedule.javazone.v3.util.LogUtils.makeLogTag;
 
 public class DigitalPassActivity extends BaseActivity {
     private static final String TAG = makeLogTag(DigitalPassActivity.class);
 
     private static final String SCREEN_LABEL = "Digital Pass";
-
 
     // intent extras used to show an arbitrary message sent via FCM
     public static final String EXTRA_DIALOG_TITLE
@@ -46,8 +31,6 @@ public class DigitalPassActivity extends BaseActivity {
             = "no.schedule.javazone.v3.EXTRA_DIALOG_NO";
     public static final String EXTRA_DIALOG_URL
             = "no.schedule.javazone.v3.EXTRA_DIALOG_URL";
-
-    private MenuItem mAvatar;
 
     private boolean mIsResumed;
 
@@ -82,7 +65,6 @@ public class DigitalPassActivity extends BaseActivity {
         return false;
     }
 
-
     @Override
     protected String getAnalyticsScreenLabel() {
         return SCREEN_LABEL;
@@ -98,7 +80,6 @@ public class DigitalPassActivity extends BaseActivity {
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Log.d(TAG, "onCreate: digitalpass");
         setContentView(R.layout.digital_pass_act);
         setFullscreenLayout();
     }
@@ -107,7 +88,6 @@ public class DigitalPassActivity extends BaseActivity {
     protected void onResume() {
         super.onResume();
         mIsResumed = true;
-        showAnnouncementDialogIfNeeded(getIntent());
     }
 
     @Override
@@ -120,126 +100,6 @@ public class DigitalPassActivity extends BaseActivity {
     protected void onNewIntent(Intent intent) {
         super.onNewIntent(intent);
         setIntent(intent);
-        showAnnouncementDialogIfNeeded(intent);
     }
 
-    @Override
-    public boolean onPrepareOptionsMenu(Menu menu) {
-        mAvatar = menu.findItem(R.id.menu_avatar);
-        return super.onPrepareOptionsMenu(menu);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        super.onCreateOptionsMenu(menu);
-        getMenuInflater().inflate(R.menu.my_schedule, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        if (item.getItemId() == R.id.menu_avatar) {
-            showDialogFragment();
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
-    // -- Auth
-
-    private void showAvatar() {
-        if (mAvatar == null) {
-            // Attempt to update avatar image, but avatar view doesn't exist yet
-            return;
-        }
-
-        mAvatar.setTitle(R.string.description_avatar);
-//        Uri photoUrl = AccountUtils.getActiveAccountPhotoUrl(this);
-//        if (photoUrl == null) {
-//            return;
-//        }
-//        Glide.with(this).load(photoUrl.toString()).asBitmap()
-//                .into(new SimpleTarget<Bitmap>(100, 100) {
-//                    @Override
-//                    public void onResourceReady(Bitmap resource,
-//                                                GlideAnimation glideAnimation) {
-//                        if (mAvatar == null) {
-//                            return;
-//                        }
-//                        RoundedBitmapDrawable circularBitmapDrawable =
-//                                RoundedBitmapDrawableFactory.create(getResources(), resource);
-//                        circularBitmapDrawable.setCircular(true);
-//                        mAvatar.setIcon(circularBitmapDrawable);
-//                    }
-//                });
-    }
-
-    void showDialogFragment() {
-        FragmentManager fm = getSupportFragmentManager();
-        MyScheduleDialogFragment myIODialogFragment = MyScheduleDialogFragment.newInstance();
-        myIODialogFragment.show(fm, "my_io_signed_in_dialog_frag");
-    }
-
-
-    // -- Announcement dialog. TODO this may no longer be used
-
-    private void showAnnouncementDialogIfNeeded(Intent intent) {
-        if (!mIsResumed) {
-            // we are called from onResume, so defer until then
-            return;
-        }
-
-        final String title = intent.getStringExtra(EXTRA_DIALOG_TITLE);
-        final String message = intent.getStringExtra(EXTRA_DIALOG_MESSAGE);
-
-        if (!TextUtils.isEmpty(title) && !TextUtils.isEmpty(message)) {
-            final String yes = intent.getStringExtra(EXTRA_DIALOG_YES);
-            final String no = intent.getStringExtra(EXTRA_DIALOG_NO);
-            final String url = intent.getStringExtra(EXTRA_DIALOG_URL);
-            LOGD(TAG, String.format(
-                    "showAnnouncementDialog: {\ntitle: %s\nmesg: %s\nyes: %s\nno %s\nurl: %s\n}",
-                    title, message, yes, no, url));
-
-            final SpannableString spannable = new SpannableString(message == null ? "" : message);
-            Linkify.addLinks(spannable, Linkify.WEB_URLS);
-
-            AlertDialog.Builder builder = new AlertDialog.Builder(this);
-            if (!TextUtils.isEmpty(title)) {
-                builder.setTitle(title);
-            }
-            builder.setMessage(spannable);
-            if (!TextUtils.isEmpty(no)) {
-                builder.setNegativeButton(no, new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        dialog.cancel();
-                    }
-                });
-            }
-            if (!TextUtils.isEmpty(yes)) {
-                builder.setPositiveButton(yes, new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
-                        startActivity(intent);
-                    }
-                });
-            }
-            final AlertDialog dialog = builder.create();
-            dialog.show();
-            final TextView messageView = (TextView) dialog.findViewById(android.R.id.message);
-            if (messageView != null) {
-                // makes the embedded links in the text clickable, if there are any
-                messageView.setMovementMethod(LinkMovementMethod.getInstance());
-            }
-
-            // remove the extras so we don't trigger again
-            intent.removeExtra(EXTRA_DIALOG_TITLE);
-            intent.removeExtra(EXTRA_DIALOG_MESSAGE);
-            intent.removeExtra(EXTRA_DIALOG_YES);
-            intent.removeExtra(EXTRA_DIALOG_NO);
-            intent.removeExtra(EXTRA_DIALOG_URL);
-            setIntent(intent);
-        }
-    }
 }
--- JZAndroid/app/src/main/java/no/schedule/javazone/v3/digitalpass/DigitalPassFragment.java
+++ JZAndroid/app/src/main/java/no/schedule/javazone/v3/digitalpass/DigitalPassFragment.java
@@ -2,18 +2,51 @@ package no.schedule.javazone.v3.digitalpass;
 
 import android.app.Fragment;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+
+import java.io.IOException;
 
 import no.schedule.javazone.v3.R;
 
+import static no.schedule.javazone.v3.util.LogUtils.makeLogTag;
+
 public class DigitalPassFragment extends Fragment{
+
+    private static final String TAG = makeLogTag(DigitalPassFragment.class);
+
+    private CameraSource cameraSource = null;
+    private CameraSourcePreview preview;
+    private GraphicOverlay graphicOverlay;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_digital_pass, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        final Button button = (Button) getView().findViewById(R.id.scann_button);
+        preview = (CameraSourcePreview) getView().findViewById(R.id.firePreview);
+        graphicOverlay = (GraphicOverlay) getView().findViewById(R.id.fireFaceOverlay);
+        if (cameraSource == null) {
+            cameraSource = new CameraSource(this.getActivity(), graphicOverlay);
+        }
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                cameraSource.setMachineLearningFrameProcessor(new BarcodeScanningProcessor());
+                try {
+                    Log.d(TAG, "onClick: start camera");
+                    preview.start(cameraSource, graphicOverlay);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
     }
 }
