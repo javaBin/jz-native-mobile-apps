866fa5bf5ffaea6f5653b54afd7471b7fba8cacf
Nico <nicolaydmohebi@gmail.com>
2018-08-15 11:34:18 +0200

    Removed unnecessary code in DigitalPassActivity and added animation to the fragment

--- JZAndroid/app/src/main/java/no/schedule/javazone/v3/digitalpass/DigitalPassFragment.java
+++ JZAndroid/app/src/main/java/no/schedule/javazone/v3/digitalpass/DigitalPassFragment.java
@@ -1,10 +1,14 @@
 package no.schedule.javazone.v3.digitalpass;
 
 import android.app.Fragment;
+import android.graphics.drawable.AnimatedVectorDrawable;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import no.schedule.javazone.v3.R;
 
@@ -15,5 +19,16 @@ public class DigitalPassFragment extends Fragment{
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_digital_pass, container, false);
+    }
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        View header = view.findViewById(R.id.header_anim);
+        if (header instanceof ImageView) {
+            AnimatedVectorDrawable avd = (AnimatedVectorDrawable) ContextCompat.getDrawable(
+                    getContext(), R.drawable.avd_header_my_io);
+            ((ImageView) header).setImageDrawable(avd);
+            avd.start();
+        }
     }
 }
